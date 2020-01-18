eee98f15f8ae8c0bf6a6d11f5e06e8471fcdc546
José Tobias <jose.tobias@outlook.com>
2020-01-18 16:37:22 -0300

    fix: merge issues
    Signed-off-by: José Tobias <jose.tobias@outlook.com>

--- system_config/install-lib.sh
+++ system_config/install-lib.sh
@@ -15,13 +15,8 @@ apt -y install libcairo2 libcairo2-dev
 # jags needed for "rjags"
 apt -y install jags
 
-<<<<<<< HEAD
-# needed for gsl pacakges of c
-apt -y install libgsl-dev
-=======
 # needed for gsl pacakges
 apt -y install libgsl-dev
 
 # libxml2
 apt -y install libxml2 libxml2-utils libxml2-dev
->>>>>>> 3591eaa48a98fc742b3ba6600199c8387b3cbc12
